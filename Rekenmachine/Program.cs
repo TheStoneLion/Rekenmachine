e4965afbfe6951a5a3ab618b3d25fbbc50ebb0b4
Marc Lowenstein <marc.lowenstein@gmail.com>
2017-04-26 17:41:46 +0200

    Oefening om een simpele rekenmackine te maken

---
+++ Rekenmachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rekenmachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
